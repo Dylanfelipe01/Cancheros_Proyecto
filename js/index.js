3f7ffa5183da8a6901edc0fde336ed693cd7b878
SebastianSPM <sebasperemald04@gmail.com>
2026-08-31 21:56:29 -0500

    Arreglando preloader y rutas

--- js/index.js
+++ js/index.js
@@ -10,3 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
+const preloaderVisto = localStorage.getItem("preloaderVisto");
+
+if (!preloaderVisto) {
+    localStorage.setItem("preloaderVisto", "true");
+    window.location.href = "./html/preloader.html";
+}
